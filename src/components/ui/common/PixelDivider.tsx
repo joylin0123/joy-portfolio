24e7ded7d02bcd6970b2b8f9d2b2e1f0cc4dd70c
joylin0123 <linjoy6@gmail.com>
2025-10-31 22:05:11 +0100

    fix: update pixel divider to fix hydration issue

--- src/components/ui/common/PixelDivider.tsx
+++ src/components/ui/common/PixelDivider.tsx
@@ -1,11 +1,9 @@
 export default function PixelDivider() {
   return (
     <div
-      className="
-        h-1 w-full my-4 opacity-40
-        text-ring
-        bg-[repeating-linear-gradient(90deg,currentColor_0_10px,transparent_10px_20px)]
-      "
+      role="separator"
+      aria-orientation="horizontal"
+      className="h-1 w-full my-4 opacity-40 text-ring bg-[repeating-linear-gradient(90deg,currentColor_0_10px,transparent_10px_20px)]"
     />
   );
 }
